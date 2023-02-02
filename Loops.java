69d53a893c4bf22e52c821879bfc7fe5c48059ea
satya <satya143@gmail.com>
2023-02-02 14:08:39 +0530

    all type of loop here

---
+++ Loops.java
@@ -0,0 +1,136 @@
+package com.company;
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args){
+
+ // while loop concept
+
+/*        int i=1;
+        while(i<=3){
+    System.out.println(i);
+    i++;
+}
+System.out.println("cydguhij");
+    }} */
+
+// do while loop
+
+/* int b=0;
+do {
+    System.out.println(b);
+    b++;
+}while(b<5);
+}}*/
+
+//for loop concept
+/*for(int i=1;i<=10;i++) {
+    System.out.println(i);
+}}}*/
+
+
+//question wap first n odd number
+
+
+/*Scanner sc=new Scanner(System.in);
+        System.out.println("first n odd numbers is ");
+int n=sc.nextInt();
+for(int i=0;i<=n;i++){
+   // System.out.println(2*i+1);//for odd number
+    System.out.println(2*i);//for even  number
+}}}*/
+
+
+   // decrementing for loop
+ /*   for(int i=5;i>=0;i--){
+        System.out.println(i);
+    }}}*/
+
+//Question 1   print the * in decarising order
+/*int n=4;
+for(int i=4;i>0;i--){
+    for(int j=0;j<i;j++){
+    System.out.print("*");}
+    System.out.println("\n");
+}}}*/
+
+// question 2
+int n=5;
+int sum=0;
+int i=0;
+while(i<=n){
+    sum=sum+ 2*i;
+
+    i++;}
+        System.out.println(sum);}
+        }
+
+        //multification of table
+      /*  int n=2;
+        System.out.println("table of 2");
+        for(int i=1;i<=10;i++){
+
+        System.out.printf("%d * %d = %d\n",n,i,n*i);
+        System.out.println();
+        // Question no 3 reverse table of 5
+
+        int n=10;
+        System.out.println("table of 10");
+
+        for(int i=10;i>=1;i--){
+        System.out.printf("%d * %d = %d\n",n,i,n*i);
+        System.out.println();}*/
+
+        //question 5 factorial
+      /*  int n =5;
+          int i=1;
+        int factorial=1;
+        while(i<=n){
+        factorial *=i;
+        i++;
+        }
+        System.out.println(factorial);}}*/
+
+        //Question no 6 for loop factorial
+
+       /* int factorial=1;
+        for(int i=3;i>=1;i--){
+        factorial*=i;
+        System.out.println(factorial);}
+
+        //multification of table
+        int n=2;
+        System.out.println("table of 2");
+        for(int i=1;i<=10;i++){
+        System.out.printf("%d * %d = %d\n",n,i,n*i);
+        System.out.println();*/
+
+
+        // Question no 3 reverse table of 5
+
+       /* int n=10;
+        System.out.println("table of 10");
+
+        for(int i=10;i>=1;i--){
+       System.out.printf("%d * %d = %d\n",n,i,n*i);
+        System.out.println();}
+        */
+
+
+        //question 5 factorial
+
+      /*  int n =5;
+
+        int i=1;
+        int factorial=1;
+        while(i<=n){
+        factorial *=i;
+        i++;
+        }
+        System.out.println(factorial);}}*/
+
+        //Question no 6 for loop factorial
+     /*   int factorial=1;
+        for(int i=3;i>=1;i--){
+        factorial*=i;
+        System.out.println(factorial);}*/
